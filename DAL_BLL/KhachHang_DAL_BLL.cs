c5034b6fd8ca0c607418ae93c2a878e3a609c57b
vietsaclo <vietsaclo@gmail.com>
2020-07-27 11:00:59 +0700

    update

---
+++ DAL_BLL/KhachHang_DAL_BLL.cs
@@ -0,0 +1,166 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+namespace DAL_BLL
+{
+    public class KhachHang_DAL_BLL
+    {
+        private QL_MBTBDTDataContext db;
+        private DonHang_DAL_BLL dbBill;
+        public KhachHang_DAL_BLL()
+        {
+            db = new QL_MBTBDTDataContext();
+            dbBill = new DonHang_DAL_BLL();
+        }
+
+        #region Phần Của Việt
+
+        public List<KhachHang_DTO> lay5KH_GanDay()
+        {
+            List<KhachHang_DTO> result = new List<KhachHang_DTO>();
+            var list = db.KhachHangs.OrderByDescending(n => n.NGAYTAO).Take(5);
+            foreach (KhachHang kh in list)
+                result.Add(new KhachHang_DTO
+                {
+                    MaKH = kh.MaKH,
+                    TaiKhoan = kh.TaiKhoan,
+                    DiaChi = kh.DiaChi,
+                    DienThoai1 = kh.DienThoai,
+                    GioiTinh = kh.GioiTinh,
+                    HoTen = kh.HoTen,
+                    MatKhau = kh.MatKhau,
+                    Status = kh.Status,
+                    Email = kh.Email
+                });
+
+            return result;
+        }
+
+        public KhachHang_DTO layKhachHang(int maKH)
+        {
+            KhachHang kh = db.KhachHangs.FirstOrDefault(n => n.MaKH.Equals(maKH));
+            if (kh == null)
+                return null;
+            return new KhachHang_DTO
+            {
+                MaKH = kh.MaKH,
+                TaiKhoan = kh.TaiKhoan,
+                DiaChi = kh.DiaChi,
+                DienThoai1 = kh.DienThoai,
+                GioiTinh = kh.GioiTinh,
+                HoTen = kh.HoTen,
+                MatKhau = kh.MatKhau,
+                Status = kh.Status,
+                Email = kh.Email
+            };
+        }
+
+        #endregion
+
+        #region Phần Của Long
+        public bool KiemTraDinhDangNgay(string s)
+        {
+            return DateTime.TryParseExact(s, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out _);
+        }
+
+        public List<KhachHang> LayKhachHangs()
+        {
+            return db.KhachHangs.ToList();
+        }
+        public EStatus XoaKhachHang(int id)
+        {
+
+            var kh = db.KhachHangs.SingleOrDefault(x => x.MaKH == id);
+            if (kh == null)
+            {
+                return EStatus.THAT_BAI;
+            }
+
+            var layHoaDon = dbBill.LayDonHangs(id);
+            if (layHoaDon.Any())
+            {
+                if (!dbBill.XoaHoaDon(layHoaDon))
+                {
+                    return EStatus.THAT_BAI;
+                }
+            }
+
+            db.KhachHangs.DeleteOnSubmit(kh);
+
+            return EStatus.THANH_CONG;
+
+        }
+        public void SaveChanged()
+        {
+            db.SubmitChanges();
+        }
+        public EStatus UpdateKhachHang(KhachHang_DTO khDto)
+        {
+            var kh = db.KhachHangs.SingleOrDefault(x => x.TaiKhoan == khDto.TaiKhoan);
+            if (kh == null) return EStatus.LOI;
+            kh.TaiKhoan = khDto.TaiKhoan;
+            kh.MatKhau = khDto.MatKhau;
+            kh.Email = khDto.Email;
+            kh.Status = khDto.Status;
+            kh.HoTen = khDto.HoTen;
+            kh.DiaChi = khDto.DiaChi;
+            kh.DienThoai = khDto.DienThoai1;
+            kh.GioiTinh = khDto.GioiTinh;
+            kh.NGAYTAO = DateTime.Now;
+            kh.NgaySinh = khDto.NgaySinh;
+            return EStatus.THANH_CONG;
+        }
+        public bool GetById(string username)
+        {
+            return db.KhachHangs.Any(x => x.TaiKhoan.Trim().Equals(username.Trim()));
+        }
+        public bool EmailIsValid(string emailAddress)
+        {
+            try
+            {
+                var mailAddress = new MailAddress(emailAddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+        public EStatus AddKhachHang(KhachHang_DTO khDto)
+        {
+            try
+            {
+
+                db.KhachHangs.InsertOnSubmit(new KhachHang()
+                {
+                    TaiKhoan = khDto.TaiKhoan,
+                    MatKhau = khDto.MatKhau,
+                    Email = khDto.Email,
+                    Status = khDto.Status,
+                    HoTen = khDto.HoTen,
+                    DiaChi = khDto.DiaChi,
+                    DienThoai = khDto.DienThoai1,
+                    GioiTinh = khDto.GioiTinh,
+                    NGAYTAO = DateTime.Now,
+                    NgaySinh = khDto.NgaySinh
+            });
+                return EStatus.THANH_CONG;
+            }
+            catch (Exception)
+            {
+                return EStatus.LOI;
+            }
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ MyLibrary/FunctionStatic.cs
@@ -0,0 +1,131 @@
+﻿using DevExpress.XtraGrid.Views.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyLibrary.fdPopup;
+using System.IO;
+using DevExpress.XtraBars.Ribbon;
+
+namespace MyLibrary
+{
+    public class FunctionStatic
+    {
+        public const string imgPolder = @"../../Contents/Images/";
+        public const string imgNotFound = "imgNotFound.jpg";
+        public const string GIOI_THIEU_UNG_DUNG =
+            "TRƯỜNG ĐẠI HỌC CÔNG NGHIỆP THỰC PHẨM\n" +
+            "THÀNH PHỐ HỒ CHÍ MINH\n" +
+            "Đồ án môn công nghệ phần mềm\n" +
+            "Năm Học 2020\n" +
+            "CÁC THÀNH VIÊN TRONG NHÓM\n" +
+            "1. NGUYỄN QUỐC VIỆT\n" +
+            "2. BÙI VŨ TRƯỜNG\n" +
+            "3. TRẦN BẢO LONG\n" +
+            "4. LÝ XUÂN THÀNH\n" +
+            "5. NGUYỄN TRẦN GIA TUẤN";
+
+        public const string GIOI_THIEU_TRUONG = 
+            "TRƯỜNG ĐẠI HỌC CÔNG NGHIỆP THỰC PHẨM\n" +
+            "THÀNH PHỐ HỒ CHÍ MINH";
+
+        public const string GIOI_THIEU_TEN_MON = 
+            "Đồ án môn công nghệ phần mềm\n" +
+            "Năm Học 2020";
+
+        public const string GIOI_THIEU_THANH_VIEN =
+            "CÁC THÀNH VIÊN TRONG NHÓM\n" +
+            "1. NGUYỄN QUỐC VIỆT\n" +
+            "2. BÙI VŨ TRƯỜNG\n" +
+            "3. TRẦN BẢO LONG\n" +
+            "4. LÝ XUÂN THÀNH\n" +
+            "5. NGUYỄN TRẦN GIA TUẤN";
+
+        public static void hienThiFormMoi(Form frmCha, Form frmDich)
+        {
+            foreach (Form f in frmCha.MdiChildren)
+                if (f.GetType() == frmDich.GetType())
+                    f.Close();
+
+            if (frmDich.Name.Equals("frmBanHang") || frmDich.Name.Equals("frmThongTin"))
+            {
+                RibbonControl rid = frmCha.Controls["ribbonMain"] as RibbonControl;
+                rid.Minimized = true;
+            }
+            else
+            {
+                RibbonControl rid = frmCha.Controls["ribbonMain"] as RibbonControl;
+                rid.Minimized = false;
+            }
+            frmDich.MdiParent = frmCha;
+            frmDich.Show();
+        }
+
+        public static string layTextGridView(GridView gridView,int row, int col)
+        {
+            if (row < 0 || row >= gridView.RowCount)
+                return null;
+            var text = gridView.GetRowCellValue(row, gridView.Columns[col]);
+            return text == null ? null : text.ToString();
+        }
+
+        public static void hienThiThongBaoLoi(string mess)
+        {
+            MessageBox.Show(mess, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void hienThiThongBaoThanhCong(string mess)
+        {
+            MessageBox.Show(mess, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static DialogResult hienThiCauHoiYesNo(string mess)
+        {
+            return MessageBox.Show(mess, "Xác Nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+
+        public static string layGhiChuTuNguoiDung()
+        {
+            string ghiChu = null;
+            frmThemGhiChu frmThemGhiChu = new frmThemGhiChu();
+            DialogResult result = frmThemGhiChu.ShowDialog();
+            if (result == DialogResult.Yes)
+                ghiChu = frmThemGhiChu.GHICHU;
+
+            return ghiChu;
+        }
+
+        public static string chonFileNameHinhAnh()
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "All Files|*.*|JPEGs|*.jpg|Bitmaps|*.bmp|GIFs|*.gif";
+            DialogResult res = of.ShowDialog();
+            return res == DialogResult.OK ? of.FileName : null;
+        }
+
+        public static string copyHinh(string fNguon)
+        {
+            if (string.IsNullOrEmpty(fNguon))
+                return string.Empty;
+            DateTime dt = DateTime.Now;
+            string fDich = string.Format("sp{0}_{1}_{2}_{3}_{4}_{5}.jpg", dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
+            File.Copy(fNguon, imgPolder + fDich);
+            return fDich;
+        }
+
+        public static void xoaHinh(string tenHinh)
+        {
+            if (File.Exists(imgPolder + tenHinh))
+                File.Delete(imgPolder + tenHinh);
+        }
+
+        public static string capNhatHinh(string tenHinhCu, string fNguon)
+        {
+            if (!tenHinhCu.Equals(imgNotFound))
+                xoaHinh(tenHinhCu);
+            return copyHinh(fNguon);
+        }
+    }
+}
